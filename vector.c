6db20d93f5b4509efa9599b72be4b1d23999879b
Robert Lowe <robert.lowe@maryvillecollege.edu>
2018-05-09 18:16:45 -0400

    Added license header.

--- vector.c
+++ vector.c
@@ -1,3 +1,24 @@
+/*
+    This is a set of functions which implements a simple resizable vector.
+    (Why should the C++ STL have all the fun? #void_is_life)
+    Copyright (C) 2018  Robert Lowe <pngwen@acm.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
 #include <stdlib.h>
 #include <string.h>
 #include <vector.h>
