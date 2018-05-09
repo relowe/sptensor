9f3cab26fa6570be439b8324c33b4a924fbc8c7e
Robert Lowe <robert.lowe@maryvillecollege.edu>
2018-05-09 16:01:53 -0400

    Added a small growable vector.

---
+++ vector.c
@@ -0,0 +1,117 @@
+#include <stdlib.h>
+#include <string.h>
+
+
+/*
+ * Allocate a vector.
+ * 
+ * Parameters: element_size - The size of each vector element
+ *             capacity     - The initial capacity of the vector
+ * 
+ * Returns: The newly allocated vector
+ */
+vector *vector_alloc(size_t element_size, int capacity) 
+{
+    vector *result;
+
+    result = malloc(sizeof(vector));
+    result->size=0;
+    result->capacity=capacity;
+    result->element_size = element_size;
+    result->ar = malloc(result->element_size * result->capacity);
+
+    return result;
+}
+
+
+/*
+ * Free all the memory associated with a vector.
+ * 
+ * Parameters: v - The vector to free
+ */
+void vector_free(vector *v)
+{
+    free(v->ar);
+    free(v);
+}
+
+
+/* 
+ * Grow the vector's capacity by a factor of two.
+ * 
+ * Parameters: v - The vector to grow
+ */
+void vector_grow(vector *v)
+{
+    void *ar;
+
+    /* allocate a new vector */
+    v->capacity *= 2;
+    ar = malloc(v->capacity * v->element_size);
+  
+    /* copy the original vector's contents */
+    memcpy(ar, v->ar, v->size * v->element_size);
+
+    /* replace the original array */
+    free(v->ar);
+    v->ar = ar;
+}
+
+
+/*
+ * Append an item to the back of the vector, growing if needed.
+ * 
+ * Paremters: v    - The vector to append to
+ *            item - The item to copy into the vector
+ */
+void vector_push_back(vector *v, void *item)
+{
+    /* grow if needed */
+    if(v->size == v->capacity) {
+	vector_grow(v);
+    }
+
+    /* insert the item */
+    memcpy(v->ar + v->size * v->element_size, item, v->element_size);
+    v->size++;
+}
+
+
+/*
+ * Insert an item into a vector at index i.  Everythine 
+ * after this position is pushed one slot further down.
+ *
+ * Paraemters: v    - The vector to insert into
+ *             i    - The index of the position to be inserted
+ *             item - The item to copy into the vector
+ */
+void vector_insert(vector *v, unsigned int i, void *item)
+{
+    /* grow if needed */
+    if(v->size == v->capacity) {
+	vector_grow(v);
+    }
+
+
+    /* shift everything down */
+    memmove(VPTR(v, i+1), VPTR(v, i), (v->size - i) * v->element_size);
+
+    /* add the element at the specified position */
+    memcpy(VPTR(v, i), item, v->element_size);
+    v->size++;
+}
+
+
+/*
+ * Remove the element at index i from the vector, shifting everything
+ * After i back one position.
+ * 
+ * Parameters: v - The vector to be modified
+ *             i - The index of the item to be removed
+ */
+void vector_remove(vector *v, unsigned int i)
+{
+    /* shift everything back one position */
+    memcpy(VPTR(v, i), VPTR(v, i+1), (v->size - i - 1) * v->element_size);
+    v->size--;
+}
