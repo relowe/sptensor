254bbf34969855e0201e88b79247913fa7381220
Robert Lowe <pngwen@acm.org>
2018-06-18 15:35:18 -0400

    Switched to new allocators.

--- multiply.c
+++ multiply.c
@@ -36,7 +36,7 @@ matrix_product(tensor_view *a, tensor_view *b)
     /* compute the dimensions and allocate the tensor */
     rdim[0] = a->dim[0];
     rdim[1] = b->dim[1];
-    result = sptensor_view_alloc(sptensor_alloc(2, rdim));
+    result = sptensor_view_tensor_alloc(2, rdim);
 
     /* perform the multiplication in an O(n^2 lg n) sort of way */
     annz = TVNNZ(a);
@@ -85,7 +85,7 @@ nmode_product(unsigned int n, tensor_view *a, tensor_view *u)
     /* create the dimensions of the result and allocatethe result. */
     memmove(idx, a->dim, sizeof(sp_index_t)*a->nmodes);
     idx[n] = u->dim[0];
-    result = sptensor_view_alloc(sptensor_alloc(a->nmodes, idx));
+    result = sptensor_view_tensor_alloc(a->nmodes, idx);
 
     /* go through each index in a */
     annz = TVNNZ(a);
