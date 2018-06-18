008524d31dd768130422274245e8cd472e184b3b
Robert Lowe <pngwen@acm.org>
2018-06-18 15:39:57 -0400

    Removed matrix multiply tests (as those are in a separate program now)

--- sptensortest.c
+++ sptensortest.c
@@ -79,7 +79,7 @@ int
 main(int argc, char **argv)
 {
     sptensor *sp;
-    tensor_view *v, *vi, *vuf, *vt, *vm;
+    tensor_view *v, *vi, *vuf, *vt;
     tensor_view *vslice;
     tensor_slice_spec *slice;
     
@@ -167,14 +167,6 @@ main(int argc, char **argv)
     vt = tensor_transpose(vi, 0, 1);
     tensor_view_print(vt, 0);
     TVFREE(vt);
-    printf("\n\n");
-
-    /* test matrix product */
-    printf("Matrix Product\n");
-    vm = matrix_product(v, v);
-    tensor_view_print(vm, 0);
-    sptensor_free(vm->tns);
-    TVFREE(vm);
     printf("\n\n");
     
     /* benchmark */
