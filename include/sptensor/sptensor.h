e73a594d55f7c89b7ca6fa4c4fb145d8b4913f86
Robert Lowe <rlowe@semo.edu>
2020-10-13 15:19:09 -0400

    Cleaned up types and removed inline recommnedations for ANSI compliance

--- include/sptensor/sptensor.h
+++ include/sptensor/sptensor.h
@@ -28,6 +28,8 @@
 /* basic typedefs and struct prototypes */
 struct sptensor;
 struct sptensor_iterator;
+typedef struct sptensor sptensor_t;
+typedef struct sptensor_iterator sptensor_iterator_t;
 
 /* function prototype types */
 typedef void (*sptensor_get_f)(sptensor_t*, sptensor_index_t*, mpf_t );
@@ -39,7 +41,7 @@ typedef int (*sptensor_iterator_prev_f)(struct sptensor_iterator*);
 typedef int (*sptensor_iterator_valid_f)(struct sptensor_iterator*);
 
 /* sptensor type (base type) */
-typedef struct sptensor
+struct sptensor
 {
     int modes;
     sptensor_index_t *dim;
@@ -48,11 +50,11 @@ typedef struct sptensor
     sptensor_iterator_f iterator;
     sptensor_iterator_f nz_iterator;
     sptensor_free_f free;
-} sptensor_t;
+};
 
 
 /* sptensor iterator (base type) */
-typedef struct sptensor_iterator
+struct sptensor_iterator
 {
     sptensor_t *t;
     sptensor_index_t *index;
@@ -60,19 +62,19 @@ typedef struct sptensor_iterator
     sptensor_iterator_next_f next;
     sptensor_iterator_prev_f prev;
     sptensor_free_f free;
-} sptensor_iterator_t;
+};
 
 
-/* inline wrappers for sptensor functions */
-inline void sptensor_get(sptensor_t * t, sptensor_index_t *i, mpf_t v) { t->get(t, i, v); }
-inline void sptensor_set(sptensor_t * t, sptensor_index_t *i, mpf_t v) { t->set(t, i, v); }
-inline sptensor_iterator_t* sptensor_iterator(sptensor_t *t) { return t->iterator(t); }
-inline sptensor_iterator_t* sptensor_nz_iterator(sptensor_t *t) { return t->nz_iterator(t); }
-inline void sptensor_free(sptensor_t* t){ t->free(t); }
+/* wrappers for sptensor functions */
+void sptensor_get(sptensor_t * t, sptensor_index_t *i, mpf_t v) { t->get(t, i, v); }
+void sptensor_set(sptensor_t * t, sptensor_index_t *i, mpf_t v) { t->set(t, i, v); }
+sptensor_iterator_t* sptensor_iterator(sptensor_t *t) { return t->iterator(t); }
+sptensor_iterator_t* sptensor_nz_iterator(sptensor_t *t) { return t->nz_iterator(t); }
+void sptensor_free(sptensor_t* t){ t->free(t); }
 
-/* inline wrappers for sptensor iterator */
-inline int sptensor_iterator_next(struct sptensor_iterator* itr) { return itr->next(itr); }
-inline int sptensor_iterator_prev(struct sptensor_iterator* itr) { return itr->prev(itr); }
-inline int sptensor_iterator_valid(struct sptensor_iterator* itr) { return itr->valid(itr); }
-inline void sptensor_free(sptensor_t* t){ t->free(t); }
+/* wrappers for sptensor iterator */
+int sptensor_iterator_next(struct sptensor_iterator* itr) { return itr->next(itr); }
+int sptensor_iterator_prev(struct sptensor_iterator* itr) { return itr->prev(itr); }
+int sptensor_iterator_valid(struct sptensor_iterator* itr) { return itr->valid(itr); }
+void sptensor_iterator_free(sptensor_iterator_t* t){ t->free(t); }
 #endif
